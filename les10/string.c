d5cc4a51e6dbbf92ba447ea96c1f851075be2d60
LemonXuyc <2051468818@qq.com>
2015-12-11 19:02:21 -0800

    string and string pointer

--- les10/string.c
+++ les10/string.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
+/********************************************************************
+gdb:
+    x/10cb 0xbffff1234: display 10 bit of char from this address
+*********************************************************************/
 int main()
 {
     char str[] = "hello";
     char *str2 = "world";
     char str3[10];
     printf("input the value \n");
-    scanf("%s", str3);
+    scanf("%s", str3);   //scanf not with *str3;
     printf("str is %s\n", str);
     printf("str2 is %s\n", str2);
     printf("str3 is %s\n", str3);
