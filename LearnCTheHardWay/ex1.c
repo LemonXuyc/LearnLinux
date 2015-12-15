aebfc0d51a0ab56af350c365bf077ada518a4f88
LemonXuyc <2051468818@qq.com>
2015-12-15 07:46:56 -0800

    how to use make

--- LearnCTheHardWay/ex1.c
+++ LearnCTheHardWay/ex1.c
@@ -4,6 +4,7 @@ int main(int argc, char *argv[])
     puts("Hello world.");
     puts("make ex1 == cc ex1.c -o ex1");
     puts("CFLAGS='-Wall' make ex1 == cc -Wall ex1.c -o ex1");
+    //this command line option tells the complier cc to report all warnings;
     puts("and you will getting a warning.");    
     puts("if there are something wrong in your codes.");
     return 0;
